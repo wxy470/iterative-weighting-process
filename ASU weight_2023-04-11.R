b4430938a765610a6a1e11e750b140d8d225ea51
wxy470 <78065664+wxy470@users.noreply.github.com>
2023-04-11 17:12:30 -0400

    Update ASU weight_2023-04-11.R

--- ASU weight_2023-04-11.R
+++ ASU weight_2023-04-11.R
@@ -3,36 +3,20 @@ library(questionr)
 
 
 rm(list = ls())
-setwd("~/OneDrive - Georgia Institute of Technology/01-Research/00-Working/US-Germany comparison/ASU")
-
-wtd.table <- questionr::wtd.table
-wtd.mean <- questionr::wtd.mean
-
+setwd("~/working directory")
 
 # ==================== ASU dataset preparation ==================== 
-ASU_data <- read.csv("USAtwFULLTIME1.csv")
-
-# employment check
-table(ASU_data$worker_now) # N=57 no
-table(ASU_data$w3_empl_now) # N=928 are working full-time, there are also part-time worker & unemployed & furloughed without pay
+ASU_data <- read.csv("ASU dataset.csv") # employed only
 
 # gender
-table(ASU_data$gender)
 ASU_data$gender_cat2 <- ifelse(ASU_data$gender=='Female', 1, 0)
 table(ASU_data$gender_cat2)
 
 # age
-table(ASU_data$ageGr)
-table(ASU_data$age)
 ASU_data$age_cat4 <- car::recode(ASU_data$age, "lo:29=1; 30:44=2; 45:64=3; 65:hi=4")
 table(ASU_data$age_cat4)
 
-table(ASU_data$gender_cat2, ASU_data$age_cat4)
-
 # HH income
-table(ASU_data$EconStat)
-table(ASU_data$inc)
-
 ASU_data$inc_cat3 <- car::recode(ASU_data$inc, "'Less than $35,000'=1; '$35,000 to $74,999'=2; '$75,000 to $99,999'=2; else=3")
 table(ASU_data$inc_cat3)
 
@@ -42,20 +26,19 @@ gender_age <- matrix(c(19221912,	27925607,	30791876,	5384125,
 hhincome <- matrix(c(32146157, 52084490, 43314083))
 
 
-# ==================== weighting ==================== 
+# ==================== iterative weighting ==================== 
 wt1 <- matrix(NA, nrow = nrow(ASU_data), ncol = 1000)
 wt2 <- matrix(NA, nrow = nrow(ASU_data), ncol = 1000)
 
-wt2[,1] <- 1
-(n_US <- nrow(ASU_data))
-
+wt2[,1] <- 1 # set initial weight
+(n_US <- nrow(ASU_data)) # sample size
 
 for (k in 2:200) {
-  N_US <- sum(gender_age)
-  ratio <- n_US/N_US
-  wt_tab <- gender_age/wtd.table(x=ASU_data$gender_cat2, y=ASU_data$age_cat4, weights=wt2[,k-1]) * ratio
-  wt_max1 <- abs(max(wt_tab-1))
-  for (i in 1:n_US) {wt1[i,k] <- wt2[i,k-1] * wt_tab[ASU_data$gender_cat2[i]+1, ASU_data$age_cat4[i]]}
+  N_US <- sum(gender_age) # population size
+  ratio <- n_US/N_US # sample:population ratio
+  wt_tab <- gender_age/wtd.table(x=ASU_data$gender_cat2, y=ASU_data$age_cat4, weights=wt2[,k-1]) * ratio # cell weight factor
+  wt_max1 <- abs(max(wt_tab-1)) # identify the largest cell weight factor
+  for (i in 1:n_US) {wt1[i,k] <- wt2[i,k-1] * wt_tab[ASU_data$gender_cat2[i]+1, ASU_data$age_cat4[i]]} # update weights with the cell weight factors
   
   N_US <- sum(hhincome)
   ratio <- n_US/N_US
@@ -65,14 +48,14 @@ for (k in 2:200) {
   
   print(k)
   k_conv <- k
-  if (max(c(wt_max1, wt_max2)) < 0.000001) break
+  if (max(c(wt_max1, wt_max2)) < 0.000001) break # converge check
   
 }
 
-ASU_data$wt <- wt2[,k_conv]
+ASU_data$wt <- wt2[,k_conv] # assgin the final weight to the dataset
 summary(ASU_data$wt)
 
-
+# plot weight distributions
 ggplot(ASU_data, aes(x=wt)) + 
   geom_histogram(aes(y=..count..), 
                  color="#00a0ff", fill="#74bbf7",
@@ -86,15 +69,16 @@ ggplot(ASU_data, aes(x=wt)) +
   theme(panel.grid.minor=element_blank())
 ggsave("Figure/US_weight.png", width = 9, height = 6)
 
-# ==================== trim weight ==================== 
-median(ASU_data$wt) + 6*IQR(ASU_data$wt)
-table(ASU_data$wt > median(ASU_data$wt) + 6*IQR(ASU_data$wt))
-wt_threshold <- median(ASU_data$wt) + 6*IQR(ASU_data$wt)
 
-ASU_data$wt_trim <- ifelse(ASU_data$wt > wt_threshold, wt_threshold, ASU_data$wt)
-ASU_data$wt_trim <- ASU_data$wt_trim * n_US/sum(ASU_data$wt_trim)
+# ==================== trim weight ==================== 
+(wt_threshold <- median(ASU_data$wt) + 6*IQR(ASU_data$wt)) # calculate trimming threshold
+table(ASU_data$wt > wt_threshold) # check the number of cases with extreme weights (i.e., above the threshold)
+
+ASU_data$wt_trim <- ifelse(ASU_data$wt > wt_threshold, wt_threshold, ASU_data$wt) # assign the threshold for all extreme weights
+ASU_data$wt_trim <- ASU_data$wt_trim * n_US/sum(ASU_data$wt_trim) # rescale the weights to make the weight sum equal to sample size
 summary(ASU_data$wt_trim)
 
+# plot trimmed weights
 ggplot(ASU_data, aes(x=wt_trim)) + 
   geom_histogram(aes(y=..count..), 
                  color="#00a0ff", fill="#74bbf7",
